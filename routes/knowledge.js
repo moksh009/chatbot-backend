7033bac66c5aaa1c84c3d6c19d2759c0e816d8e8
Moksh090 <moksh@ttfaconsultancy.in>
2026-04-12 15:15:32 +0530

    n

--- routes/knowledge.js
+++ routes/knowledge.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Client = require('../models/Client');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 const log = require('../utils/logger')('KnowledgeRoute');
 
 /**
